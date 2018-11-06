e72366adb510cf40e91bdfa12714d61ab18570a2
Wim <44021592+MyBunzor@users.noreply.github.com>
2018-11-06 11:39:23 +0100

    initial commit

---
+++ app/src/main/java/com/example/wvand/tictactoe/Game.java
@@ -0,0 +1,53 @@
+package com.example.wvand.tictactoe;
+
+public class Game {
+    final private int BOARD_SIZE = 3;
+    private TileState[][] board;
+
+    private Boolean playerOneTurn;  // true if player 1's turn, false if player 2's turn
+    private int movesPlayed;
+    private Boolean gameOver;
+
+    public Game() {
+        board = new TileState[BOARD_SIZE][BOARD_SIZE];
+        for(int i=0; i<BOARD_SIZE; i++)
+            for(int j=0; j<BOARD_SIZE; j++)
+                board[i][j] = TileState.BLANK;
+
+        playerOneTurn = true;
+        gameOver = false;
+    }
+
+    public TileState choose(int row, int column) {
+
+        //TileState Bonkers = board[row][column];
+
+        // if tile is blank, fill it
+        if (TileState.BLANK == board[row][column]){
+
+
+            if (playerOneTurn == true){
+                return TileState.CROSS;
+
+                // move turn to player two
+                playerOneTurn = false;
+
+            }
+            else {
+                return TileState.CIRCLE;
+
+                // move turn back to player one
+                playerOneTurn = true;
+            }
+        else{
+                return TileState.INVALID;
+
+            }
+        }
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/wvand/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/wvand/tictactoe/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.wvand.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        game = new Game();
+    }
+
+    public void tileClicked(View view) {
+        String tilenr = String.valueOf(view.getId());
+        String[] id = tilenr.split(",");
+        int row = Integer.valueOf(id[0]);
+        int column = Integer.valueOf(id[1])
+        TileState state = game.choose(row, column);
+
+        switch(state) {
+            case CROSS:
+
+                // change text of button to X
+                view.setText("X");
+                break;
+            case CIRCLE:
+
+                // Change text of button to O
+                view.setTExt("O");
+
+                break;
+            case INVALID:
+
+                // do nothing -> prompt player to click again?
+                break;
+        }
     }
 }
